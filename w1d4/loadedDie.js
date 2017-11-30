5e2e024575d07c854bc595a113476bfe1cb3d0eb
Matthew Daniel Murphy <matt@mattmurphy.ca>
2017-11-30 18:09:30 -0500

    Completed closures exercises 1 and 2

---
+++ w1d4/loadedDie.js
@@ -0,0 +1,15 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+
+  return function() {
+    /* your code here */
+    return list.splice(0, 1);
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
--- w1d4/sortAdvanced.js
+++ w1d4/sortAdvanced.js
@@ -1,1 +1,38 @@
-sortAdvanced.js
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+	// var older = duplicates.sort(function(a, b) {
+	// 	return a.age - b.age;
+	// });
+
+function sortNames(array) {
+	// sort all names
+	var duplicates = [];
+
+	for(n in array) {
+		array.sort(function(a, b) {
+			if(a.name < b.name) {
+				return -1;
+			};
+			if(a.name > b.name) {
+				return 1;
+			};
+			return 0;
+		});
+	}
+	for(n in array) {
+		var isOlder = array.sort(function(a, b) {
+			return b.age - a.age;
+		});
+	};
+	console.log('older:', isOlder);
+	return array;
+}
+
+// sortNames(students);
+
+console.log(sortNames(students));
